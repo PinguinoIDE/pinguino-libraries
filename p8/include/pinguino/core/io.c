0b14315ed35ab88ca2d9f0c6c97657ef120434b3
regis <regis@pinguino.cc>
2016-02-08 14:31:32 +0100

    Fixed io.c for 16F1459 Serial Input

--- p8/include/pinguino/core/io.c
+++ p8/include/pinguino/core/io.c
@@ -84,10 +84,9 @@ void IO_init(void)
 
     TRISA = 0x00;
     #ifndef I2CINT
-        #if defined(__SERIAL__) && defined(__16F1459)
-        TRISB |= 0b11011111; // Clear all bits except RB5 (Serial INPUT)
-        #else
         TRISB = 0x00;
+        #if defined(__SERIAL__) && defined(__16F1459)
+        TRISBbits.TRISB5 = 1; // Clear all bits except RB5 (Serial INPUT)
         #endif
     #endif
     
